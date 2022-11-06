e426b0f0de7f27760fe70f8c80bdc8ae785068b4
holakmateusz <holak.mateusz@gmail.com>
2022-11-06 16:24:26 +0100

    Add test 'of' rx js function

--- index.ts
+++ index.ts
@@ -5,36 +5,41 @@ import axios from 'axios';
 
 // Open the console in the bottom right to see results.
 
-const controller = new AbortController();
+// const controller = new AbortController();
 
-const dataApiObservable = new Observable((subscriber) => {
-  axios
-    .get('https://random-data-api.com/api/name/random_name', {
-      signal: controller.signal,
-    })
-    .then((response) => subscriber.next(response.data.first_name));
-  return () => controller.abort();
+// const dataApiObservable = new Observable((subscriber) => {
+//   axios
+//     .get('https://random-data-api.com/api/name/random_name', {
+//       signal: controller.signal,
+//     })
+//     .then((response) => subscriber.next(response.data.first_name));
+//   return () => controller.abort();
+// });
+
+// dataApiObservable.subscribe((value) => console.log('1 subscriber: ', value));
+
+// dataApiObservable.subscribe((value) => console.log('2 subscriber: ', value));
+
+// dataApiObservable.subscribe((value) => console.log('3 subscriber: ', value));
+
+// const rxjsButton = document.querySelector('button#rxjs_test_button');
+// console.log(rxjsButton.tagName);
+
+// const buttonClickObservable = new Observable<MouseEvent>((subscriber) => {
+//   rxjsButton.addEventListener('click', (event) => {
+//     subscriber.next(event);
+//   });
+// });
+
+// buttonClickObservable.subscribe((event) =>
+//   console.log('Subscriber 1: ', event.x)
+// );
+
+// buttonClickObservable.subscribe((event) =>
+//   console.log('Subscriber 2: ', event.x)
+// );
+
+of("Alice","Ben","Marta").subscribe({
+  next:(value)=>console.log("name: ",value),
+  complete:()=>console.log("Complete")
 });
-
-dataApiObservable.subscribe((value) => console.log('1 subscriber: ', value));
-
-dataApiObservable.subscribe((value) => console.log('2 subscriber: ', value));
-
-dataApiObservable.subscribe((value) => console.log('3 subscriber: ', value));
-
-const rxjsButton = document.querySelector('button#rxjs_test_button');
-console.log(rxjsButton.tagName);
-
-const buttonClickObservable = new Observable<MouseEvent>((subscriber) => {
-  rxjsButton.addEventListener('click', (event) => {
-    subscriber.next(event);
-  });
-});
-
-buttonClickObservable.subscribe((event) =>
-  console.log('Subscriber 1: ', event.x)
-);
-
-buttonClickObservable.subscribe((event) =>
-  console.log('Subscriber 2: ', event.x)
-);
