8c973c402b6c277e0a69a286136e0b3205304606
holakmateusz <holak.mateusz@gmail.com>
2022-11-06 15:26:06 +0100

    Add example of cold observable

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import './style.css';
 
 import { of, map, Observable } from 'rxjs';
+import axios from 'axios';
 
 of('World')
   .pipe(map((name) => `Hello, ${name}!`))
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
+
+
+
+
+const dataApiObservable = new Observable((subscriber=>{
+  axios.get('https://random-data-api.com/api/name/random_name').then((response)=>subscriber.next(response.data.first_name))
+}));
+
+dataApiObservable.subscribe((value)=>console.log("1 subscriber: ",value))
+
+dataApiObservable.subscribe((value)=>console.log("2 subscriber: ",value))
+
+dataApiObservable.subscribe((value)=>console.log("3 subscriber: ",value))
