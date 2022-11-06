dcec2ab4b6d21bdb4730a2e1ef5c353b6f5166ef
holakmateusz <holak.mateusz@gmail.com>
2022-11-06 16:01:43 +0100

    Add example of hot observable

--- index.ts
+++ index.ts
@@ -3,21 +3,38 @@ import './style.css';
 import { of, map, Observable } from 'rxjs';
 import axios from 'axios';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
-  .subscribe(console.log);
-
 // Open the console in the bottom right to see results.
 
+const controller = new AbortController();
 
+const dataApiObservable = new Observable((subscriber) => {
+  axios
+    .get('https://random-data-api.com/api/name/random_name', {
+      signal: controller.signal,
+    })
+    .then((response) => subscriber.next(response.data.first_name));
+  return () => controller.abort();
+});
 
+dataApiObservable.subscribe((value) => console.log('1 subscriber: ', value));
 
-const dataApiObservable = new Observable((subscriber=>{
-  axios.get('https://random-data-api.com/api/name/random_name').then((response)=>subscriber.next(response.data.first_name))
-}));
+dataApiObservable.subscribe((value) => console.log('2 subscriber: ', value));
 
-dataApiObservable.subscribe((value)=>console.log("1 subscriber: ",value))
+dataApiObservable.subscribe((value) => console.log('3 subscriber: ', value));
 
-dataApiObservable.subscribe((value)=>console.log("2 subscriber: ",value))
+const rxjsButton = document.querySelector('button#rxjs_test_button');
+console.log(rxjsButton.tagName);
 
-dataApiObservable.subscribe((value)=>console.log("3 subscriber: ",value))
+const buttonClickObservable = new Observable<MouseEvent>((subscriber) => {
+  rxjsButton.addEventListener('click', (event) => {
+    subscriber.next(event);
+  });
+});
+
+buttonClickObservable.subscribe((event) =>
+  console.log('Subscriber 1: ', event.x)
+);
+
+buttonClickObservable.subscribe((event) =>
+  console.log('Subscriber 2: ', event.x)
+);
